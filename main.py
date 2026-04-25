7917bf4423e2e002bac3e9963e5528c92231d9bd
JuriVirtanen <denetgabriel@gmail.com>
2026-04-26 00:01:05 +0200

    pygame setup

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
 import pygame
+import os
+
+pygame.init()
+win = pygame.display.set_mode((900, 500))
+pygame.display.set_caption("Story of Laastia")
+
+kratka = pygame.image.load("frame.png")
+kratkalo = pygame.transform.scale(kratka, (100, 100))
+
+def draw():
+    win.blit(kratkalo, (0, 0))
+    pygame.display.update()
+
+def main():
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        draw()
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
